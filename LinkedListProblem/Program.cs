f1d2d68ebf21694e98c875fa7b97d637639e1082
mmkr7111 <mmkr7111@gmail.com>
2020-10-19 09:32:00 +0530

    [Manish Reddy]Add: Implemented Queue, stack and upto uc6 on linkedlist

---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedListProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List operations");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.WriteLine("search element" + list.Search(30));
+        }
+    }
+}
